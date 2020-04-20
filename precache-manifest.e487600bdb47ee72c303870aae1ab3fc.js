f7dce4be9d6c18ea02025506c978fd02de221ad0
anton-vm <anton_vm@ukr.net>
2020-04-20 21:59:04 +0300

    Updates

---
+++ precache-manifest.e487600bdb47ee72c303870aae1ab3fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8737585ef56b645ce0d15585118b021",
+    "url": "/sfd-prommo-app/index.html"
+  },
+  {
+    "revision": "351a1dc08986257ab631",
+    "url": "/sfd-prommo-app/static/css/main.3c0c40e5.chunk.css"
+  },
+  {
+    "revision": "486d81e292ae49473dbc",
+    "url": "/sfd-prommo-app/static/js/2.73748c7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sfd-prommo-app/static/js/2.73748c7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "351a1dc08986257ab631",
+    "url": "/sfd-prommo-app/static/js/main.424dfce3.chunk.js"
+  },
+  {
+    "revision": "60ac2c9db082576f6bbf",
+    "url": "/sfd-prommo-app/static/js/runtime-main.462a05ee.js"
+  }
+]);
